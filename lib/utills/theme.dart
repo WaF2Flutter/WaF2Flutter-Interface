892384b4cf33e6081448eda538df1cf85805a395
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-09-17 15:39:48 +0330

    Update theme.dart

--- lib/utills/theme.dart
+++ lib/utills/theme.dart
@@ -2,16 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//Todo: Working on WHITE THEME...
 ThemeData getTheme(bool isDark) {
   return isDark
-    ? ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color.fromARGB(255, 31, 30, 45),
-        canvasColor: Color(0xFF2A2D3E),
-        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme.apply(bodyColor: Colors.white)),
-      )
-    : ThemeData.light().copyWith(
+    ? ThemeData.light().copyWith(
         scaffoldBackgroundColor: Colors.white,
         canvasColor: Colors.white,
         textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme.apply(bodyColor: Colors.black)),
+      )
+      :ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color.fromARGB(255, 31, 30, 45),
+        canvasColor: Color(0xFF2A2D3E),
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme.apply(bodyColor: Colors.white)),
       );
 }

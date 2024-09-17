629b254bd54ebe4f4326a7d404aacd31f2f70326
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-09-17 15:35:19 +0330

    Create translator.dart

---
+++ lib/utills/translator.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+//Working ON
+class Translator extends Translations{
+  @override
+  Map<String, Map<String, String>> get keys =>
+  {
+    'en' : 
+    {
+      '' : '',
+    },
+    'fa':
+    {
+      '' : '',
+    },
+  };
+  
+  }

23811fb1589e478ce692e20d027697b5d96cc1bd
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-11 21:15:58 +0330

    Update WebSiteScreen.dart

--- lib/screens/WebSiteScreen.dart
+++ lib/screens/WebSiteScreen.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:msf/controllers/IdleController.dart';
 import 'package:msf/screens/websites/websites_screen.dart';
 import 'package:msf/utills/responsive.dart';
+import '../controllers/MenuController.dart';
 import 'component/SideBar.dart';
 import 'package:get/get.dart';
 
 class Websitescreen extends StatelessWidget {
+  final Menu_Controller menuController = Get.put(Menu_Controller());
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,12 @@ class Websitescreen extends StatelessWidget {
         Get.find<IdleController>().onUserInteraction();
       },
       child: Scaffold(
+                key: menuController.scaffoldKey, 
+          drawer: !Responsive.isDesktop(context) 
+            ? const Drawer( 
+                child: SideBar(),
+              )
+            : null,
         body: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [

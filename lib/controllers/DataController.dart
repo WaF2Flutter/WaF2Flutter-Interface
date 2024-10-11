0c9b1763e3506aa7154e2864fec1087db3071357
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-11 20:39:13 +0330

    Update DataController.dart

--- lib/controllers/DataController.dart
+++ lib/controllers/DataController.dart
@@ -1,20 +1,44 @@
+//IN DEV mode this data's are sample.
+import 'dart:async';
+import 'dart:math';
 import 'package:get/get.dart';
-import 'package:msf/data/RecentActivity.dart';
-import 'package:msf/data/data.dart';
 
-class DataController extends GetxController{
-  List<CloudInfo> _cloudInfo = demoInfo;
-  List<Recentactivity> _recentActivity = demoRecentActivity;
+class DataController extends GetxController {
 
-  List<CloudInfo> get cloudInfo => _cloudInfo;
-  List<Recentactivity> get recentActivity => _recentActivity;
+  Random _random = Random();
 
+  var cpuUsage = 75.obs;
+  var cloudUsage = 50.obs;
+  var memoryUsage = 90.obs;
+  var trafficUsage = 60.obs;
 
-  void updateData(List<CloudInfo> newCloudInfo, List<Recentactivity> newRecentActivity) {
-    _cloudInfo.clear();
-    _cloudInfo.addAll(newCloudInfo);
+  var cpuFiles = 8.obs;
+  var cloudFiles = 502.obs;
+  var memoryFiles = 15.obs;
+  var trafficFiles = 600.obs;
 
-    _recentActivity.clear();
-    _recentActivity.addAll(newRecentActivity);
+  var cpuStorage = "80%".obs;
+  var cloudStorage = "800 MB".obs;
+  var memoryStorage = "16 GB".obs;
+  var trafficStorage = "1 TB".obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _startUpdatingData(); 
+  }
+
+  void _startUpdatingData() {
+    Timer.periodic(Duration(seconds: 2), (timer) {
+      cpuUsage.value = _random.nextInt(100);
+      cloudUsage.value = _random.nextInt(100);
+      memoryUsage.value = _random.nextInt(100);
+      trafficUsage.value = _random.nextInt(100);
+
+      cpuFiles.value = _random.nextInt(50);
+      cloudFiles.value = _random.nextInt(1000);
+      memoryFiles.value = _random.nextInt(50);
+      trafficFiles.value = _random.nextInt(1000);
+    });
   }
 }

edf17201acc3c2367c03068f22fd492d54a681a4
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-11 21:18:41 +0330

    Update Header.dart

--- lib/screens/component/Header.dart
+++ lib/screens/component/Header.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:get/get.dart';
 import 'package:msf/utills/colorconfig.dart';
 import 'package:msf/utills/responsive.dart';
+import 'package:msf/controllers/MenuController.dart';
 class Header extends StatelessWidget {
+  final Menu_Controller menuController = Get.find<Menu_Controller>();
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        if(!Responsive.isDesktop(context))
-        IconButton(
-          onPressed: () {
-         //Use Getx Controller that we might use.   
-          },
-          icon: const Icon(Icons.menu_sharp),
-        ),
-        if(!Responsive.isMobile(context))
-          AutoSizeText("Welcome  Admin!",
+        if (!Responsive.isDesktop(context))
+          IconButton(
+            onPressed: () {
+                menuController.openDrawer();
+            },
+            icon: const Icon(Icons.menu_sharp),
+          ),
+        if (!Responsive.isMobile(context))
+          AutoSizeText(
+            "Welcome Admin!",
             style: Theme.of(context).textTheme.bodyLarge,
           ),
-        if(!Responsive.isMobile(context)) Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
+        if (!Responsive.isMobile(context)) Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
         Expanded(
           child: TextField(
             decoration: InputDecoration(
@@ -31,9 +35,9 @@ class Header extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
               suffixIcon: InkWell(
-                onTap: (){},
+                onTap: () {},
                 child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal:16/2),
+                  margin: const EdgeInsets.symmetric(horizontal: 16 / 2),
                   padding: const EdgeInsets.all(16 * 0.75),
                   decoration: const BoxDecoration(
                     color: primaryColor,
@@ -49,18 +53,21 @@ class Header extends StatelessWidget {
           margin: const EdgeInsets.only(left: 16),
           padding: const EdgeInsets.symmetric(
             horizontal: 16,
-            vertical: 16/2,
+            vertical: 16 / 2,
           ),
-          decoration:BoxDecoration(color: secondryColor,
-            borderRadius:BorderRadius.circular(10), border: Border.all(color: Colors.white12),),
+          decoration: BoxDecoration(
+            color: secondryColor,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.white12),
+          ),
           child: Row(
             children: [
-              const Icon(Icons.account_circle,size: 26,),
+              const Icon(Icons.account_circle, size: 26),
               const Text("admin1"),
-              IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_drop_down_rounded),)
+              IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_drop_down_rounded)),
             ],
           ),
-        )
+        ),
       ],
     );
   }

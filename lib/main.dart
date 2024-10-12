ba2dbb135bf2cae68c70693ebefd12a71dfac0e2
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-12 19:19:01 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:msf/bindings/bindings.dart';
-import 'package:msf/controllers/IdleController.dart';
-import 'package:msf/controllers/ThemeController.dart';
-import 'package:msf/screens/HomeScreen.dart';
 import 'package:get/get.dart';
+import 'package:msf/screens/HomeScreen.dart';
 import 'package:msf/screens/LoginScreen.dart';
 import 'package:msf/screens/WebSiteScreen.dart';
 import 'package:msf/utills/theme.dart';
 import 'package:msf/utills/translator.dart';
+import 'package:msf/controllers/ThemeController.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Get.put(ThemeController()); 
   runApp(MyApp());
 }
 
@@ -17,8 +18,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      Get.put(ThemeController());
-      Get.put(IdleController()); 
       bool isDark = Get.find<ThemeController>().isDark.value;
 
       return GetMaterialApp(
@@ -27,13 +26,12 @@ class MyApp extends StatelessWidget {
         initialBinding: MyBindings(),
         getPages: [
           GetPage(name: '/home', page: () => HomeScreen()),
-          GetPage(name: '/websites',page: ()=>Websitescreen()),
+          GetPage(name: '/websites', page: () => Websitescreen()),
           GetPage(name: '/l', page: () => LoginScreen()),
         ],
-        initialRoute: '/l',
+        initialRoute: '/home', 
         title: 'ModSec Admin Panel'.tr,
-        theme: getTheme(isDark),
-        home: HomeScreen(),
+        theme: getTheme(isDark), 
       );
     });
   }

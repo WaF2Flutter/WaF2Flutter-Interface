f2c7e11feb78e475dfed5974c3918430e437a91a
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-20 21:37:28 +0330

    Update Header.dart

--- lib/screens/component/Header.dart
+++ lib/screens/component/Header.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:get/get.dart';
+import 'package:msf/controllers/CounterController.dart';
 import 'package:msf/utills/colorconfig.dart';
 import 'package:msf/utills/responsive.dart';
 import 'package:msf/controllers/MenuController.dart';
 
 class Header extends StatelessWidget {
-  final GlobalKey<ScaffoldState> scaffoldKey; 
-  final Menu_Controller menuController = Get.find<Menu_Controller>(); 
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  final Menu_Controller menuController = Get.find<Menu_Controller>();
 
-  Header({required this.scaffoldKey}); 
+  Header({required this.scaffoldKey});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +19,7 @@ class Header extends StatelessWidget {
         if (!Responsive.isDesktop(context))
           IconButton(
             onPressed: () {
-              menuController.openDrawer(scaffoldKey); 
+              menuController.openDrawer(scaffoldKey);
             },
             icon: const Icon(Icons.menu_sharp),
           ),
@@ -27,30 +28,24 @@ class Header extends StatelessWidget {
             "Welcome Admin!".tr,
             style: Theme.of(context).textTheme.bodyLarge,
           ),
-        if (!Responsive.isMobile(context)) Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
-        Expanded(
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: "Search".tr,
-              fillColor: secondryColor,
-              filled: true,
-              border: const OutlineInputBorder(
-                borderSide: BorderSide.none,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              suffixIcon: InkWell(
-                onTap: () {},
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 16 / 2),
-                  padding: const EdgeInsets.all(16 * 0.75),
-                  decoration: const BoxDecoration(
-                    color: primaryColor,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  child: const Icon(Icons.search),
+        if (!Responsive.isMobile(context))
+          Spacer(flex: Responsive.isDesktop(context) ? 1 : 2),
+          Spacer(),
+        Container(
+          width: 200,
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: secondryColor
+          ),
+          child: Obx(
+            () {
+              return Center(
+                child: Text(
+                  "Time remaining:   ".tr + Get.find<Counter>().remainingSec,
                 ),
-              ),
-            ),
+              );
+            },
           ),
         ),
         Container(
@@ -67,8 +62,7 @@ class Header extends StatelessWidget {
           child: Row(
             children: [
               const Icon(Icons.account_circle, size: 26),
-              const Text("admin1"),
-
+              const Text("test"),
               PopupMenuButton<String>(
                 color: secondryColor,
                 onSelected: (value) {
@@ -111,7 +105,7 @@ class Header extends StatelessWidget {
         ),
         content: Text(
           "Are you sure you want to logout?".tr,
-          style: TextStyle(color: Colors.white), 
+          style: TextStyle(color: Colors.white),
         ),
         actions: [
           TextButton(
@@ -120,12 +114,12 @@ class Header extends StatelessWidget {
             },
             child: Text(
               "Cancel".tr,
-              style: TextStyle(color: Colors.red), 
+              style: TextStyle(color: Colors.red),
             ),
           ),
           TextButton(
             onPressed: () {
-              Get.back(); 
+              Get.back();
             },
             child: Text("Confirm".tr),
           ),

e3b12b20372b20ca491ed550eabfd0748af7fb93
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-20 21:36:14 +0330

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:msf/bindings/bindings.dart';
+import 'package:get/get.dart';
+import 'package:msf/controllers/ThemeController.dart';
+import 'package:msf/screens/HomeScreen.dart';
+import 'package:msf/screens/LoginScreen.dart';
+import 'package:msf/screens/SettingScreen.dart';
+import 'package:msf/screens/WebSiteScreen.dart';
+import 'package:msf/utills/theme.dart';
+import 'package:msf/utills/translator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Get.put(ThemeController()); 
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      bool isDark = Get.find<ThemeController>().isDark.value;
+
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        translations: Translator(),
+        initialBinding: MyBindings(),
+        getPages: [
+          GetPage(name: '/home', page: () => HomeScreen()),
+          GetPage(name: '/websites', page: () => Websitescreen()),
+          GetPage(name: '/setting', page: () => Settingscreen()),
+          GetPage(name: '/l', page: () => LoginScreen()),
+        ],
+        initialRoute: '/home', 
+        title: 'ModSec Admin Panel'.tr,
+        theme: getTheme(isDark), 
+      );
+    });
+  }
+}

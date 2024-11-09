e0b796b350a61b531d9b88f980df54a0d9900fe8
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-11-09 20:36:04 +0330

    Update com.dart

--- lib/unit/com.dart
+++ lib/unit/com.dart
@@ -1,122 +1,139 @@
-
-import 'package:http/http.dart' as http;
+import 'dart:async';
 import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-class Communication {
+import '../controllers/WsConnection.dart';
 
-  final String apiUrl;
-  late WebSocketChannel webSocketChannel;
-  String? privateKey;
+class Com {
+  bool _loginSuccess = false;
+  String? sessionId;
+  WebSocketChannel? channel;
+  bool _isConnected = false;
 
-  Communication(this.apiUrl);
 
-  Future<bool> login(String username, String password) async {
-    final response = await http.post(
-      Uri.parse('$apiUrl/auth/login'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'username': username, 'password': password}),
-    );
+  bool get isConnected => _isConnected;
 
-    if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
-      privateKey = data['privateKey'];
+  Future<bool> wsConnect() async {
+    try {
+      channel = WebSocketChannel.connect(Uri.parse("ws://127.0.0.1:8081/ws"));
+
+      channel!.stream.listen(
+            (message) {
+          print("Received data: $message");
+          Get.find<WsConnection>().processIncomingData(message);
+        },
+        onError: (error) {
+          print("WebSocket connection error: $error");
+          _isConnected = false;
+          channel?.sink.close();
+        },
+        onDone: () {
+          print("WebSocket connection closed.");
+          _isConnected = false;
+        },
+      );
+
+      _isConnected = true;
       return true;
-    } else {
-      throw Exception('Failed to authenticate user');
+    } catch (e) {
+      print("Failed to connect: $e");
+      _isConnected = false;
+      return false;
     }
   }
 
-  void connectWebSocket() {
-    webSocketChannel = WebSocketChannel.connect(Uri.parse('$apiUrl/socket'));
-    webSocketChannel.stream.listen((message) {
-      print('Received: $message');
-    }, onError: (error) {
-      print('WebSocket Error: $error');
-    });
+  void sendMessage(String message) {
+    if (_isConnected && channel != null) {
+      channel!.sink.add(message);
+      print("Sent message: $message");
+    } else {
+      print('Cannot send message: WebSocket is not connected.');
+    }
   }
 
-  Future<bool> sendLoginSession(String username, String password) async {
-    bool isAuthenticated = await login(username, password);
-    if (isAuthenticated) {
-      connectWebSocket();
-      return true;
+
+
+
+  Future<void> login(String username, String password) async {
+    String url = 'http://127.0.0.1:8081/login';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'username': username,
+          'password': password,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        if (jsonData['login_status'] == 'pending') {
+          sessionId = jsonData['id'];
+          print("OTP sent, session id: $sessionId");
+        } else {
+          print("Unexpected login status: ${jsonData['login_status']}");
+        }
+      } else {
+        print("Login failed with status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error during login request: $e");
+    }
+  }
+
+  Future<bool> verifyOtp(int otp) async {
+    String url = 'http://127.0.0.1:8081/verify_otp';
+    if (sessionId == null) {
+      print("ID not found");
+      return false;
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'session_id': sessionId!,
+          'otp': otp,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        if (jsonData['login_status'] == 'success') {
+          print("login success");
+          _loginSuccess = true;
+          return true;
+        } else {
+          print("Unexpected response: ${jsonData['login_status']}");
+        }
+      } else {
+        print(
+            "OTP verification failed with status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error during OTP verification: $e");
     }
     return false;
   }
 
-  void sendMessage(String message) {
-    webSocketChannel.sink.add(message);
-  }
 
-  void dispose() {
-    webSocketChannel.sink.close();
-  }
 }
-/*
-import 'dart:async';
-import 'dart:convert';
 
-import 'package:web_socket_channel/web_socket_channel.dart';
-
-//import 'package:msf/controllers/ResourceUsageController.dart';
-
-class Communication {
-  late WebSocketChannel _webSocket;
-  bool _isConnected = false;
-  bool _loginSuccess = false;
-  final _responseStreamController = StreamController<String>.broadcast();
-
-
-  void establishWebSocketConnection() {
-    try {
-      //Url for connection.
-      String url = "ws://26.68.236.137:8080";
-      _webSocket = WebSocketChannel.connect(Uri.parse(url));
-      _webSocket.stream.listen((message) {
-        _responseStreamController.add(message);
-      }, onError: (error) {
-        print('Error occurred: $error');
-        _isConnected = false;
-      }, onDone: () {
-        print('WebSocket connection closed');
-        _isConnected = false;
-      });
-
-      _isConnected = true;
-    } catch (e) {
-      print('Error establishing WebSocket connection: $e');
-    }
-  }
-  Future<bool?> sendLoginSession(String username, String password) async {
-    if (_isConnected) {
-      _webSocket.sink.add('-login' + username + password);
-      final response = await _responseStreamController.stream.first;
-      if (response.isNotEmpty) {
-        final jsonData = jsonDecode(response);
-        if (jsonData['login_status'] == 'success') {
-          print("success");
-          return true;
-        } else {
-          return false;
-        }
-      }
-    } else {
-      print('Cannot create login session: webSocket is not connected.');
-      return false;
-    }
-  }
-  bool isConnected() {
-    return _isConnected;
-  }
-}
-//Todo: its usable usefull for communication between server and app
+// Todo: You can add methods to send messages or handle WebSocket disconnect
 /*
   void sendMessage(String message) {
     if (_isConnected) {
       _webSocket.sink.add(message);
     } else {
-      print('Cannot send message: webSocket is not connected.');
+      print('Cannot send message: WebSocket is not connected.');
     }
   }
 
@@ -126,5 +143,4 @@ class Communication {
       _isConnected = false;
     }
   }
-  */
   */

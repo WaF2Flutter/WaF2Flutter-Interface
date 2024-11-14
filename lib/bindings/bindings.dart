d143cde485b5333bdcc1ea3df4b0d7872f79a64b
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-11-14 23:39:09 +0330

    Update bindings.dart

--- lib/bindings/bindings.dart
+++ lib/bindings/bindings.dart
@@ -1,27 +1,27 @@
 import 'package:get/get.dart';
-import 'package:msf/controllers/CounterController.dart';
-import 'package:msf/controllers/LoginController.dart';
-import 'package:msf/controllers/MenuController.dart';
-import 'package:msf/controllers/ThemeController.dart';
-import 'package:msf/controllers/TranslateController.dart';
-import 'package:msf/controllers/IdleController.dart';
-import 'package:msf/controllers/ResourceUsageController.dart';
-import '../controllers/WsConnection.dart';
+import 'package:msf/controllers/auth/CaptchaController.dart';
+import 'package:msf/controllers/dashboard/CounterController.dart';
+import 'package:msf/controllers/auth/LoginController.dart';
+import 'package:msf/controllers/settings/MenuController.dart';
+import 'package:msf/controllers/settings/ThemeController.dart';
+import 'package:msf/controllers/settings/TranslateController.dart';
+import 'package:msf/controllers/settings/IdleController.dart';
+import 'package:msf/controllers/dashboard/ResourceUsageController.dart';
+import '../controllers/ws/WsController.dart';
 
 
 class MyBindings implements Bindings {
   @override
   void dependencies() {
-    print("Initializing MyBindings...");
+    Get.put(ThemeController());
     Get.lazyPut(() => LoginController());
     Get.lazyPut(() => Menu_Controller());
-    Get.lazyPut(() => ThemeController());
     Get.put(Counter());
     Get.lazyPut(() => TranslateController());
     Get.put(IdleController());
     Get.lazyPut(() => ResourceUsageController());
-    Get.put(WsConnection());
-    print("MyBindings initialized.");
+    Get.put(WsController());
+    Get.lazyPut(()=>CaptchaController());
   }
 }
 
